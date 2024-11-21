6df36e6e445c7d3cfa4b25d69856af451b12fcad
Scott Lancaster <github@capitalsmart.com.au>
2024-11-21 12:07:01 +1100

    test

---
+++ application/views/login/test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>i-S.M.A.R.T | Login</title>
+	
+</head>
+
+<body>
+
+echo base_url
+
+
+</body>
+</html>

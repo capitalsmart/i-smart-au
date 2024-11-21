2669a4c988c1a316059b0b1929c0a99ff6892aee
Scott Lancaster <github@capitalsmart.com.au>
2024-11-21 13:52:19 +1100

    revet changes, add new folder under application root

--- application/controllers/FileUploader.php
+++ application/controllers/FileUploader.php
@@ -1,20 +1,15 @@
- <?php
+<?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class FileUploader extends CI_Controller {
 
- 
- public function __construct() {
-        // Ensure session is started
-        if (session_status() == PHP_SESSION_NONE) {
-            session_start();
-        }
-
-        // Check if user is logged in and handle AJAX requests
-       /*  if ($this->session->userdata("isUserLoggedIn") && !isset($_REQUEST['isAjax'])) {
+    public function __construct() {
+        parent::__construct();
+	/*
+         if (!$this->session->userdata("isUserLoggedIn") && !isset($_REQUEST['isAjax'])) {
             redirect('/Login/index', 'refresh');
-        } elseif (!$this->session->userdata("isUserLoggedIn") && isset($_REQUEST['isAjax'])) {
+        } elseif(!$this->session->userdata("isUserLoggedIn") && isset($_REQUEST['isAjax'])) {
             $response = [
                 'redirectUrl' => '/Login/index',
                 'message' => 'Not Authorized',
@@ -24,46 +19,51 @@ class FileUploader extends CI_Controller {
             exit;
         }
 		*/
-
         $this->load->library("fineuploader");
         $this->load->helper("fineuploader");
     }
 
     public function index() {
-        // Check if user UUID is set
-        if ($this->session->userdata("uuid-user") === null) {
+       /* if ($this->session->userdata("uuid-user") === null) {
             header('HTTP/1.1 401 Unauthorized', true, 401);
             exit();
         }
+		*/
 
         $this->fineuploader->allowedExtensions = ['jpeg', 'jpg', 'png'];
-        $this->fineuploader->sizeLimit = 2000000; // 2 MB
+        $this->fineuploader->sizeLimit = 2000000; //2 mb
         $uploadDir = 'assets/uploads/' . $this->session->userdata("uuid-user");
-        $method = $_SERVER['REQUEST_METHOD'];
+        $method = get_request_method();
 
         if ($method == "POST") {
             header("Content-Type: text/plain");
-
+            // Assumes you have a chunking.success.endpoint set to point here with a query parameter of "done".
+            // For example: /myserver/handlers/endpoint.php?done
             if (isset($_GET["done"])) {
                 $result = $this->fineuploader->combineChunks("files");
-            } else {
+            }
+            // Handles upload requests
+            else {
+                // Call handleUpload() with the name of the folder, relative to PHP's getcwd()
                 if (!file_exists($uploadDir)) {
                     mkdir($uploadDir, 0777, true);
                 }
                 $result = $this->fineuploader->handleUpload($uploadDir);
+                // To return a name used for uploaded file you can use the following line.
                 $result["uploadName"] = $this->fineuploader->getUploadName();
-                $files = $this->session->userdata("user-files") == null ? [] : $this->session->userdata("user-files");
+                $files = $this->session->userdata("user-files") == null ? [] :$this->session->userdata("user-files");
                 $files[$result['uuid']] = $result['uploadName'];
                 $this->session->set_userdata("user-files", $files);
             }
             echo json_encode($result);
-        } elseif ($method == "DELETE") {
+        }// for delete file requests
+        else if ($method == "DELETE") {
             $result = $this->fineuploader->handleDelete($uploadDir);
-            $tempDir = 'assets/uploads/' . $this->session->userdata("uuid-user") . "/" . $result['uuid'];
+            $tempDir = 'assets/uploads/'.$this->session->userdata("uuid-user")."/".$result['uuid'];
             if (file_exists($tempDir)) {
                 deleteDir($tempDir);
             }
-            $files = $this->session->userdata("user-files") == null ? [] : $this->session->userdata("user-files");
+            $files = $this->session->userdata("user-files") == null ? [] :$this->session->userdata("user-files");
             unset($files[$result['uuid']]);
             $this->session->set_userdata("user-files", $files);
             echo json_encode($result);
--- application/controllers/test_FileUploader.php
+++ application/controllers/test_FileUploader.php
@@ -1,15 +1,20 @@
-<?php
+ <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class FileUploader extends CI_Controller {
 
-    public function __construct() {
-        parent::__construct();
-	/*
-         if (!$this->session->userdata("isUserLoggedIn") && !isset($_REQUEST['isAjax'])) {
+ 
+ public function __construct() {
+        // Ensure session is started
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        // Check if user is logged in and handle AJAX requests
+       /*  if ($this->session->userdata("isUserLoggedIn") && !isset($_REQUEST['isAjax'])) {
             redirect('/Login/index', 'refresh');
-        } elseif(!$this->session->userdata("isUserLoggedIn") && isset($_REQUEST['isAjax'])) {
+        } elseif (!$this->session->userdata("isUserLoggedIn") && isset($_REQUEST['isAjax'])) {
             $response = [
                 'redirectUrl' => '/Login/index',
                 'message' => 'Not Authorized',
@@ -19,51 +24,46 @@ class FileUploader extends CI_Controller {
             exit;
         }
 		*/
+
         $this->load->library("fineuploader");
         $this->load->helper("fineuploader");
     }
 
     public function index() {
-       /* if ($this->session->userdata("uuid-user") === null) {
+        // Check if user UUID is set
+        if ($this->session->userdata("uuid-user") === null) {
             header('HTTP/1.1 401 Unauthorized', true, 401);
             exit();
         }
-		*/
 
         $this->fineuploader->allowedExtensions = ['jpeg', 'jpg', 'png'];
-        $this->fineuploader->sizeLimit = 2000000; //2 mb
+        $this->fineuploader->sizeLimit = 2000000; // 2 MB
         $uploadDir = 'assets/uploads/' . $this->session->userdata("uuid-user");
-        $method = get_request_method();
+        $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method == "POST") {
             header("Content-Type: text/plain");
-            // Assumes you have a chunking.success.endpoint set to point here with a query parameter of "done".
-            // For example: /myserver/handlers/endpoint.php?done
+
             if (isset($_GET["done"])) {
                 $result = $this->fineuploader->combineChunks("files");
-            }
-            // Handles upload requests
-            else {
-                // Call handleUpload() with the name of the folder, relative to PHP's getcwd()
+            } else {
                 if (!file_exists($uploadDir)) {
                     mkdir($uploadDir, 0777, true);
                 }
                 $result = $this->fineuploader->handleUpload($uploadDir);
-                // To return a name used for uploaded file you can use the following line.
                 $result["uploadName"] = $this->fineuploader->getUploadName();
-                $files = $this->session->userdata("user-files") == null ? [] :$this->session->userdata("user-files");
+                $files = $this->session->userdata("user-files") == null ? [] : $this->session->userdata("user-files");
                 $files[$result['uuid']] = $result['uploadName'];
                 $this->session->set_userdata("user-files", $files);
             }
             echo json_encode($result);
-        }// for delete file requests
-        else if ($method == "DELETE") {
+        } elseif ($method == "DELETE") {
             $result = $this->fineuploader->handleDelete($uploadDir);
-            $tempDir = 'assets/uploads/'.$this->session->userdata("uuid-user")."/".$result['uuid'];
+            $tempDir = 'assets/uploads/' . $this->session->userdata("uuid-user") . "/" . $result['uuid'];
             if (file_exists($tempDir)) {
                 deleteDir($tempDir);
             }
-            $files = $this->session->userdata("user-files") == null ? [] :$this->session->userdata("user-files");
+            $files = $this->session->userdata("user-files") == null ? [] : $this->session->userdata("user-files");
             unset($files[$result['uuid']]);
             $this->session->set_userdata("user-files", $files);
             echo json_encode($result);
